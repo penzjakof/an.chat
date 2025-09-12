7d76b39b438e253266e5c187a10ce25f0ee1a073
AnChat Server <server@anchat.me>
2025-09-13 00:21:39 +0300

    fix(api): register DatameImportController in DatameModule

--- apps/server/src/datame/datame.module.ts
+++ apps/server/src/datame/datame.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatameService } from './datame.service';
 import { DatameController } from './datame.controller';
+import { DatameImportController } from './datame.controller';
 import { HttpModule } from '../common/http/http.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { EncryptionModule } from '../common/encryption/encryption.module';
@@ -8,7 +9,7 @@ import { DatameImportService } from './datame.import.service';
 
 @Module({
   imports: [HttpModule, PrismaModule, EncryptionModule],
-  controllers: [DatameController],
+  controllers: [DatameController, DatameImportController],
   providers: [DatameService, DatameImportService],
   exports: [DatameService, DatameImportService],
 })
