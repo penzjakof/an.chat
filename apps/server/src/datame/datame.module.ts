2c3b54223208f704f0fc71790bab5a9933b7632e
AnChat Server <server@anchat.me>
2025-09-13 11:55:53 +0300

    fix(server): додати AuthModule до DatameModule для JwtService в JwtAuthGuard

--- apps/server/src/datame/datame.module.ts
+++ apps/server/src/datame/datame.module.ts
@@ -6,9 +6,10 @@ import { HttpModule } from '../common/http/http.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { EncryptionModule } from '../common/encryption/encryption.module';
 import { DatameImportService } from './datame.import.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [HttpModule, PrismaModule, EncryptionModule],
+  imports: [HttpModule, PrismaModule, EncryptionModule, AuthModule],
   controllers: [DatameController, DatameImportController],
   providers: [DatameService, DatameImportService],
   exports: [DatameService, DatameImportService],
